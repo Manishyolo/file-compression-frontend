d3f039096858c879b137521d15941055f228fe97
Manish kumar <manish04kumarsingh@gmail.com>
2026-09-18 15:56:02 +0530

    Enhance file upload form: add resolution options and open download link in new tab

--- src/App.jsx
+++ src/App.jsx
@@ -28,15 +28,17 @@ const App = () => {
     <div>
        <form action="submit" onSubmit={handleupload}>
          <select name='resolution' id='resolution'>
+             <option value="Default">Default</option>
           <option value="640 x 360">640 x 360</option>
           <option value="4096 x 2160">4096 x 2160</option>
+          <option value="1920 x 1080">1920 x 1080</option>
         </select>
         <input type="file" placeholder='upload your file here' name='file'/>
        
         <button type='submit'>Upload</button>
        </form>
          {
-           downloadUrl ? <a href={downloadUrl} download>Download file</a> : ""
+           downloadUrl ? <a target='blank' href={downloadUrl} download>Download file</a> : ""
          }
 
       
